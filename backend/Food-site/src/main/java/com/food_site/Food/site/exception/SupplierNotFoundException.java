a26a9f8cfbc1b650f4b8b506389109083772562b
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-02-09 05:43:33 +0530

    Delete backend/Food-site/src/main/java/com/food_site/Food/site/exception/SupplierNotFoundException.java

--- backend/Food-site/src/main/java/com/food_site/Food/site/exception/SupplierNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.food_site.Food.site.exception;
-
-public class SupplierNotFoundException extends RuntimeException{
-    public SupplierNotFoundException(Long id){
-        super("Could not found the supplier id: "+id);
-    }
-}
