5358ca982e191ce7e32934f5471ba807e19906ca
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-02-09 05:43:50 +0530

    Delete backend/Food-site/src/main/java/com/food_site/Food/site/repository/SupplierRepo.java

--- backend/Food-site/src/main/java/com/food_site/Food/site/repository/SupplierRepo.java
+++
@@ -1,7 +0,0 @@
-package com.food_site.Food.site.repository;
-
-import com.food_site.Food.site.entity.supplier.FoodSupplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepo extends JpaRepository<FoodSupplier,Long> {
-}
