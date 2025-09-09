a1b0729e9a9ea91c6cd6b21f9c8a80c0f88eeb78
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-09-09 23:20:17 +0530

    update hero section

--- foodsite/src/App.js
+++ foodsite/src/App.js
@@ -1,36 +1,48 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Navbar and Footer
 import Navbar from './component/Navbar/Navbar';
+import Footer from './component/Navbar/Footer';
+
+// Home Components
 import Hero from './component/Hero/Hero';
 import Services from './component/Services/Services';
 import Banner from './component/Services/Banner';
 import AppStore from './component/Services/AppStore';
 import Comments from './component/Services/Comments';
-import Footer from './component/Navbar/Footer';
+import Cart from './component/Dishes/Cart';
+
+// Auth & User Management
 import Login from './component/Register/Login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct import for BrowserRouter
 import SignUp from './component/Register/SignUp';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import Role from './component/Register/Role';
 import UserService from './component/Register/UserService';
 import UserManagement from './component/Register/UserManagement';
-import Role from './component/Register/Role';
-import Admin from './component/Admin/Admin';
-import Supplier from './component/Supplier/Supplier';
-import Cart from './component/Dishes/Cart';
-import FoodList from './component/Dishes/FoodList';
-import AddFood from './component/Supplier/AddFood';
-import Payment from './component/Payment/Payment';
-import ViewFood from './component/Supplier/ViewFood';
-import Profile from './component/Profile/Profile';
-import Contact from './component/Contact/Contact';
-import OrderSummary from './component/Dishes/OrderSummary';
 import EditUser from './component/Admin/EditUser';
 import ViewProfile from './component/Admin/ViewProfile';
 
+// Admin & Supplier
+import Admin from './component/Admin/Admin';
+import Supplier from './component/Supplier/Supplier';
 
+// Food Management
+import FoodList from './component/Dishes/FoodList';
+import AddFood from './component/Supplier/AddFood';
+import ViewFood from './component/Supplier/ViewFood';
+import Payment from './component/Payment/Payment';
+
+// Other Pages
+import Profile from './component/Profile/Profile';
+import Contact from './component/Contact/Contact';
+import OrderSummary from './component/Dishes/OrderSummary';
 
 export default function App() {
+  const showNavbar = true; // Show navbar on all pages, can be made dynamic
+
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -38,54 +50,59 @@ export default function App() {
       easing: 'ease-in-sine',
       delay: 100,
     });
-
-    AOS.refresh(); // Refresh animations
+    AOS.refresh();
   }, []);
 
   return (
-    <Router> {/* Wrap everything in the Router */}
+    <BrowserRouter>
+      {showNavbar && <Navbar />}
+
       <Routes>
-        {/* Home Route */}
+        {/* Home Page */}
         <Route
           path="/"
           element={
-            <div className="bg-white dark:">
-              {/* Main page components */}
-              <Navbar />
-              <Hero />
-              <Cart/>
-              <Services />
-              <Banner />
-              <AppStore />
-              <Comments />
-              <Footer />
-            </div>
+            <>
+              <div id="hero"><Hero /></div>
+              <div id="cart"><Cart /></div>
+              <div id="services"><Services /></div>
+              <div id="banner"><Banner /></div>
+              <div id="appstore"><AppStore /></div>
+              <div id="comments"><Comments /></div>
+              <div id="footer"><Footer /></div>
+            </>
           }
         />
-        
-        {/* Login Route */}
+
+        {/* Auth & Role */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/role" element={<Role />} />
+
+        {/* Admin & Supplier */}
         <Route path="/admin" element={<Admin />} />
         <Route path="/supplier" element={<Supplier />} />
+
+        {/* Food Management */}
         <Route path="/foodlist" element={<FoodList />} />
         <Route path="/add" element={<AddFood />} />
-        <Route path="/payment" element={<Payment />} />
         <Route path="/viewfood" element={<ViewFood />} />
+        <Route path="/payment" element={<Payment />} />
+
+        {/* Profile & Contact */}
         <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/orsummery" element={<OrderSummary />} />
-        
+
+        {/* Admin-only routes */}
         {UserService.adminOnly() && (
           <>
             <Route path="/user" element={<UserManagement />} />
             <Route path="/edit/:userId" element={<EditUser />} />
             <Route path="/view/:userId" element={<ViewProfile />} />
           </>
-        
         )}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
